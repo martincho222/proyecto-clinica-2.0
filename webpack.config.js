3f47e9a22311c672321f7ea895e2a6f904d272ef
martin jerez <martincho111986@gmail.com>
2020-02-18 03:00:41 -0300

    Nuevo repositorio creado

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-proposal-class-properties']]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader, // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions
+          options: {
+            plugins: function () { // postcss plugins, can be exported to postcss.config.js
+              return [
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/seccionUsuario.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ]
+};
