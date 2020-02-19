e157a4fa798a7efc1739778859e9eb69ee933660
unknown <rosarioscarlata24@gmail.com>
2020-02-19 15:55:39 -0300

    footer

--- webpack.config.js
+++ webpack.config.js
@@ -3,65 +3,64 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: {
-    main: "./src/index.js",
-    datos: "./src/seccionUsuario.js"
-  },
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [['@babel/plugin-proposal-class-properties']]
-          }
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.(scss)$/,
-        use: [{
-          loader: MiniCssExtractPlugin.loader, // inject CSS to page
-        }, {
-          loader: 'css-loader', // translates CSS into CommonJS modules
-        }, {
-          loader: 'postcss-loader', // Run postcss actions
-          options: {
-            plugins: function () { // postcss plugins, can be exported to postcss.config.js
-              return [
-                require('autoprefixer')
-              ];
-            }
-          }
-        }, {
-          loader: 'sass-loader' // compiles Sass to CSS
-        }]
-      },
-    ]
-  },
+    entry: {
+        main: "./src/index.js",
+        datos: "./src/seccionUsuario.js"
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "build")
+    },
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            ['@babel/plugin-proposal-class-properties']
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader, // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run postcss actions
+                    options: {
+                        plugins: function() { // postcss plugins, can be exported to postcss.config.js
+                            return [
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            },
+        ]
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/seccionUsuario.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles.css"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "seccionUsuario.css"
-    })
-  ]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        //
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "seccionUsuario.css"
+        })
+    ]
 };
