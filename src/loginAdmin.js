a3faf6c7564111eceb2ef9093717a5ff1c4f12b2
martin jerez <martincho111986@gmail.com>
2020-03-13 19:22:13 -0300

    guardando cambios para realizar pull de navbar

--- src/loginAdmin.js
+++ src/loginAdmin.js
@@ -1,172 +1,129 @@
 // import 'bootstrap';
 // import './index.scss';
 
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});	
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
 
 // INICIO LOGIN Y REGISTRO DE ADMINISTRADOR.-
-  
 
 var db = firebase.firestore();
-  
 
-	// registro de nuevos usuarios
-	let nombres,
-	 apellidos,
-	 email,
-	 password,
-	 matriculaProf,
-	 profesion;
+// registro de nuevos usuarios
+let nombres, apellidos, email, password, matriculaProf, profesion;
 
-// const Registro ={
-// 	nombre: [],
-// 	apellido: [],
-// 	email: [],
-// 	contraseña: [],
-// 	matricula: [],
-// 	profesion: []
-// }
+// const email2 = 'lala@kalima.com.ar';
 
-	// Registro.nombre.push(nombres);
-		// Registro.apellido.push(apellidos);
-		// Registro.email.push(email);
-		// Registro.contraseña.push(password);
-		// Registro.matricula.push(matriculaProf);
-		// Registro.profesion.push(profesion);
+document
+  .getElementById("formAdmin")
+  .addEventListener("submit", validarRegistroAdmin);
 
+function validarRegistroAdmin(e) {
+  e.preventDefault();
+  nombres = document.getElementById("nombresAdm").value;
+  apellidos = document.getElementById("apellidosAdm").value;
+  email = document.getElementById("emailAdm").value;
+  password = document.getElementById("passwordAdm").value;
+  matriculaProf = document.getElementById("matriculaProfAdm").value;
+  profesion = document.getElementById("profesionAdm").value;
+  const isValidEmail = /(\w+)@kalima.com.ar/gi.test(email);
+  if (isValidEmail) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
 
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
 
+    db.collection("admin")
+      .add({
+        nombre: nombres,
+        apellido: apellidos,
+        email: email,
+        password: password,
+        matricula: matriculaProf,
+        especialidad: profesion,
+        historia: ""
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
 
-		// const email2 = 'lala@kalima.com.ar';
-		const isValidEmail = /(\w+)@kalima.com.ar/gi.test(email);
-		if(email === isValidEmail){}
+      .catch(function(error) {
+        // console.error("Error adding document: ", error);
+      });
+  } else {
+    //   console.log('email no valido');
+  }
+  document.getElementById("nombresAdm").value = "";
+  document.getElementById("apellidosAdm").value = "";
+  document.getElementById("emailAdm").value = "";
+  document.getElementById("passwordAdm").value = "";
+  document.getElementById("matriculaProfAdm").value = "";
+  document.getElementById("profesionAdm").value = "";
+}
 
-		
+function logear() {
+  let userAdmin = document.getElementById("adminEmail").value;
+  let userPass = document.getElementById("adminPassword").value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userAdmin, userPass)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
 
-document.getElementById('registroAdmin')
-	.addEventListener('click', ()=>{
+  observador();
+}
 
-		nombres = document.getElementById('nombresAdm').value;
-		apellidos = document.getElementById('apellidosAdm').value;
-		email = document.getElementById('emailAdm').value;
-		password = document.getElementById('passwordAdm').value;
-		matriculaProf = document.getElementById('matriculaProfAdm').value;
-		profesion = document.getElementById('profesionAdm').value;
-		
+var contenido = document.getElementById("contenido");
+function observador() {
+  console.log("verificando");
+  firebase.auth().onAuthStateChanged(function(admin) {
+    if (admin) {
+      console.log("existe usuario activo");
+      // ingresarDatos();
 
-		
-			firebase.auth().createUserWithEmailAndPassword(email, password)
-			.catch(function(error) {
-			// Handle Errors here.
-			var errorCode = error.code;
-			var errorMessage = error.message;
-			
-			console.log(errorCode);
-			console.log(errorMessage);
-		  });
-
-		  db.collection("admin").add({
-			nombre: nombres,
-			apellido: apellidos,
-			email: email,
-			password: password,
-			matricula: matriculaProf,
-			especialidad: profesion,
-			historia: "",
-			
-		  
-		  
-		  })
-		  .then(function(docRef) {
-			console.log(isValidEmail);
-			console.log("Document written with ID: ", docRef.id);
-		  })
-		  .catch(function(error) {
-			console.error("Error adding document: ", error);
-		  });
-
-			
-		
-	})
-
-
-
-
-function logear(){
-	
-	let userAdmin = document.getElementById('adminEmail').value;
-	let userPass = document.getElementById('adminPassword').value;
-        // console.log('presionando');
-	// if (userAdmin === email  && userPass === password){ 
-	// 		document.formAdmin.submit(); 
-	// 		firebase.auth().signInWithEmailAndPassword(userAdmin, userPass)
-	// 		.catch(function(error) {
-	// 			// Handle Errors here.
-	// 			var errorCode = error.code;
-	// 			var errorMessage = error.message;
-	// 			debugger;
-	// 			console.log(errorCode);
-	// 			console.log(errorMessage);
-	// 			...
-	// 			document.getElementById('inicioSesion').value;
-	// 	  });
-	// 	} 
-	// 	else{ 
-	// 		 alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
-	// 	} 
-	firebase.auth().signInWithEmailAndPassword(userAdmin, userPass).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// ...
-		console.log(errorCode);
-		console.log(errorMessage);
-	  });
-
-	  observador ();
-
-	} 
-	
-	var contenido = document.getElementById('contenido');
-function observador (){
-	console.log('verificando');
-	firebase.auth().onAuthStateChanged(function(admin) {
-		if (admin) {
-			console.log('existe usuario activo');
-			// ingresarDatos();
-			
-			// redireccionar();
-		  // User is signed in.
-		  var displayName = admin.displayName;
-		  var email = admin.email;
-		  var emailVerified = admin.emailVerified;
-		  var photoURL = admin.photoURL;
-		  var isAnonymous = admin.isAnonymous;
-		  var uid = admin.uid;
-		  var providerData = admin.providerData;
-		  contenido.style.setProperty('color', '#2daf18')
-		  contenido.innerHTML = `Usuario Registrado`
-			location.href = "adm.html"
-		  // ...
-		//   localStorage.clear();
-		} else {
-		  // User is signed out.
-		  console.log('no existe usuario activo');
-		  contenido.style.setProperty('color', '#f81321')
-		  contenido.innerHTML = `Usuario no Registrado`;
-		  // ...
-		}
-		
-	  });
+      // redireccionar();
+      // User is signed in.
+      var displayName = admin.displayName;
+      var email = admin.email;
+      var emailVerified = admin.emailVerified;
+      var photoURL = admin.photoURL;
+      var isAnonymous = admin.isAnonymous;
+      var uid = admin.uid;
+      var providerData = admin.providerData;
+      contenido.style.setProperty("color", "#2daf18");
+      contenido.innerHTML = `Usuario Registrado`;
+      location.href = "adm.html";
+      // ...
+      //   localStorage.clear();
+    } else {
+      // User is signed out.
+      console.log("no existe usuario activo");
+      contenido.style.setProperty("color", "#f81321");
+      contenido.innerHTML = `Usuario no Registrado`;
+      // ...
+    }
+  });
 }
 
 // FIN DEL JS PARA EL LOGIN Y REGISTRO DE ADMINISTRADOR.-
--- src/loginPaciente.js
+++ src/loginPaciente.js
@@ -1,91 +1,91 @@
 var db = firebase.firestore();
 
-function registrar(){
- let email = document.getElementById('email').value;
- let nombre = document.getElementById('nombre').value;
- let apellido = document.getElementById('apellido').value;
- let contrasena = document.getElementById('contrasena').value;
+function registrar() {
+  let email = document.getElementById("email").value;
+  let nombre = document.getElementById("nombre").value;
+  let apellido = document.getElementById("apellido").value;
+  let contrasena = document.getElementById("contrasena").value;
 
- firebase.auth().createUserWithEmailAndPassword(email, contrasena).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-    // ...
-  });
-
-  db.collection("user").add({
-    nombre: nombre,
-    apellido: apellido,
-    email: email,
-    password: contrasena,
-    historia: '',
-    ObraSocial: '',
-    ObraSocialN: '',
-    OrdenesAprob: '' 
-    
-    
-    })
-    .then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-    })
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, contrasena)
     .catch(function(error) {
-    console.error("Error adding document: ", error);
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // ...
     });
 
+  db.collection("user")
+    .add({
+      nombre: nombre,
+      apellido: apellido,
+      email: email,
+      password: contrasena,
+      historia: "",
+      ObraSocial: "",
+      ObraSocialN: "",
+      OrdenesAprob: ""
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
 
-  document.getElementById('email').value = "";
-  document.getElementById('nombre').value ="";
-  document.getElementById('apellido').value ="";
-  document.getElementById('contrasena').value ="";
-
-  
-
+  document.getElementById("email").value = "";
+  document.getElementById("nombre").value = "";
+  document.getElementById("apellido").value = "";
+  document.getElementById("contrasena").value = "";
 }
 
-function logIn(){
-    let email2 = document.getElementById('email2').value;
-    let contra2 = document.getElementById('contra2').value;
+function logIn() {
+  let email2 = document.getElementById("email2").value;
+  let contra2 = document.getElementById("contra2").value;
 
-    firebase.auth().signInWithEmailAndPassword(email2, contra2).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
-       
-        // console.log("usuario si esta logueado y puede ingresar");
-        
-        observador ();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email2, contra2)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+
+  // console.log("usuario si esta logueado y puede ingresar");
+
+  observador();
 }
 
-function observador (){
-	console.log('verificando');
-	firebase.auth().onAuthStateChanged(function(user) {
-		if (user) {
-			console.log('existe usuario activo');
-			// ingresarDatos();
-			
-			// redireccionar();
-		  // User is signed in.
-		  var displayName = user.displayName;
-		  var email = user.email;
-		  var emailVerified = user.emailVerified;
-		  var photoURL = user.photoURL;
-		  var isAnonymous = user.isAnonymous;
-		  var uid = user.uid;
-		  var providerData = user.providerData;
-		  
-			location.href = "seccionUsuario.html"
-		  // ...
-		//   localStorage.clear();
-		} else {
-		  // User is signed out.
-		  console.log('no existe usuario activo');
-		  // ...
-		}
-		
-	  });
+function observador() {
+  console.log("verificando");
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log("existe usuario activo");
+      // ingresarDatos();
+
+      // redireccionar();
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+
+      location.href = "seccionUsuario.html";
+      // ...
+      //   localStorage.clear();
+    } else {
+      // User is signed out.
+      console.log("no existe usuario activo");
+      // ...
+    }
+  });
 }
